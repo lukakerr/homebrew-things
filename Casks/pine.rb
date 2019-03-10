932240ebf56b6e9cdb71ebe7a69c2c0931bc74ca
lukakerr <lukakerr1@gmail.com>
2019-03-10 16:47:41 +1100

    Update Pine to 0.0.8

--- Casks/pine.rb
+++ Casks/pine.rb
@@ -1,6 +1,6 @@
 cask 'pine' do
-  version '0.0.7'
-  sha256 '90721c736a467c0171e2098b57f3006fc770714cd468e6cd8fd9b39ccf62466f'
+  version '0.0.8'
+  sha256 'e3fc06454e4c32e6dbcad5cfb86259aad2851cbc8d8ad6dd192fbb2857ba21fa'
 
   url "https://github.com/lukakerr/pine/releases/download/#{version}/Pine-#{version}.zip"
   appcast 'https://github.com/lukakerr/pine/releases.atom'
